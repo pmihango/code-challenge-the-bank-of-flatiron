1fbf28c7901417a7f598fe9c7191482fd4bff465
pmihango <pmihango@gmail.com>
2023-01-17 07:28:53 +0300

    amended the import css on index.js

---
+++ src/components/App.js
@@ -0,0 +1,42 @@
+
+import React from "react";
+import AccountContainer from "./AccountContainer";
+
+function App() {
+  return (
+    <div className="ui raised segment">
+      <div className="ui segment violet inverted">
+        <h2>The Bank of Flatiron</h2>
+      </div>
+      <AccountContainer />
+    </div>
+  );
+}
+
+export default App;
+
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
