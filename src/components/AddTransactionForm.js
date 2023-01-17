8ccad24b863cdc472e66c9d0ae40cc88191cfaf1
pmihango <pmihango@gmail.com>
2023-01-17 09:22:46 +0300

    initial commit

--- src/components/AddTransactionForm.js
+++ src/components/AddTransactionForm.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import Transaction from "./Transaction";
+// import Transaction from "./Transaction";
 
 function AddTransactionForm() {
   const [date, setDate] = useState("")
@@ -7,7 +7,7 @@ function AddTransactionForm() {
   const [category, setCategory] = useState("")
   const [amount, setAmount] = useState("")
   function handleSubmit(e) {
-    fetch("http://localhost:8001/transactions", {
+    fetch("http://localhost:3001/transactions", {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="ui raised segment">
       <div className="ui segment violet inverted">
-        <h2>The Bank of Flatiron</h2>
+        <h2> Bank of Flatiron</h2>
       </div>
       <AccountContainer />
     
